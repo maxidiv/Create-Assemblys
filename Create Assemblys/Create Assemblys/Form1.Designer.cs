9440edf8ca087e403b914e455287136ed5642d95
maxidiv <maksim.novik@gmail.com>
2014-12-09 21:42:25 +0300

    залил проект VS2013

---
+++ Create Assemblys/Create Assemblys/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ButtonRemove = new System.Windows.Forms.Button();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Ok
+            // 
+            this.Ok.Location = new System.Drawing.Point(103, 209);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(75, 23);
+            this.Ok.TabIndex = 0;
+            this.Ok.Text = "Ok";
+            this.Ok.UseVisualStyleBackColor = true;
+            this.Ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Отметьте изделие Master";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(44, 65);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(175, 116);
+            this.listBox1.TabIndex = 3;
+            // 
+            // ButtonRemove
+            // 
+            this.ButtonRemove.Location = new System.Drawing.Point(239, 94);
+            this.ButtonRemove.Name = "ButtonRemove";
+            this.ButtonRemove.Size = new System.Drawing.Size(31, 23);
+            this.ButtonRemove.TabIndex = 4;
+            this.ButtonRemove.Text = "-";
+            this.ButtonRemove.UseVisualStyleBackColor = true;
+            this.ButtonRemove.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Location = new System.Drawing.Point(239, 65);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(31, 23);
+            this.ButtonAdd.TabIndex = 5;
+            this.ButtonAdd.Text = "+";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.ButtonRemove);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ok);
+            this.Name = "Form1";
+            this.Text = "Создание сборки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button ButtonRemove;
+        private System.Windows.Forms.Button ButtonAdd;
+    }
+}
+
---
+++ Create Assemblys/Create Assemblys/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using e3;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            
+        }
+
+        public void AddToListBox()
+        {
+            app = ConnectToE3();
+            job = CreateJobObject();
+            dev = job.CreateDeviceObject();
+            dynamic devids1=null;
+            dynamic devids2=null;
+            
+            int nDev1 = job.GetSelectedDeviceIds(ref devids1);
+            int nDev2 = job.GetTreeSelectedDeviceIds(ref devids2);
+            int  [] slctDevs=new int [nDev1+nDev2];
+            for (int i = 0; i < nDev1; i++)
+            {
+                slctDevs[i] = devids1[i+1]; 
+            }
+            for (int k = 0; k < nDev2; k++)
+            {
+                slctDevs[k + nDev1] = devids2[k+1];
+            }
+
+            string[] myList = new string[(nDev1+nDev2)];
+            for (int j = 0; j < nDev1+nDev2; j++)
+            {
+                dev.SetId(slctDevs[j]);
+                myList[j] = "" + dev.GetComponentName();
+            }
+            listBox1.Items.AddRange(myList);
+            
+            
+            app = null;
+            job = null;
+            dev = null;
+        }
+        
+
+        e3Application app;
+        e3Job job;
+        e3Device dev;
+        e3Device dev1;
+        e3Device dev2;
+        e3Symbol sym;
+        e3Sheet sht;
+        e3Pin pin;
+        e3Pin pin1;
+        e3Pin pin2;
+        e3Pin cor;
+        e3Text txt;
+        e3Job CreateJobObject()
+        {
+            // возвращаемое функцией значение
+            e3Job ret = null;
+            try
+            {
+                ret = (e3Job)app.CreateJobObject();
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Невозможно создать объект Job в E3.Series" +
+                    "\nСообщение: " + ex.Message +
+                    "\n\nВыход из программы");
+                System.Environment.Exit(0);
+            }
+
+            // нет открытых проектов - выход
+            if (ret.GetId() == 0)
+            {
+                MessageBox.Show("Нет открытых проектов" +
+                    "\n\nВыход из программы");
+                System.Environment.Exit(0);
+            }
+
+            return ret;
+        }
+        e3Application ConnectToE3()
+        {
+            // получаем количество процессов E3.series
+            int processCount =
+                System.Diagnostics.Process.GetProcessesByName("E3.series").Length;
+
+            switch (processCount)
+            {
+                case 0: // нет запущенных процессов
+                    MessageBox.Show("Нет запущенных окон E3.Series" +
+                        "\n\nВыход из программы");
+                    System.Environment.Exit(0);
+                    break;
+                case 1: // запущен один процесс
+                    try
+                    {
+                        return (e3Application)System.Activator.CreateInstance
+                            (System.Type.GetTypeFromProgID("CT.Application"));
+                    }
+                    catch (System.Exception)
+                    {
+                        MessageBox.Show("Не установлен E3.Series" +
+                            "\n\nВыход из программы");
+                    }
+                    break;
+                default: // запущено несколько процессов
+                    dynamic ret = null;
+
+                    try
+                    {
+                        dynamic Viewer = System.Activator.CreateInstance
+                        (System.Type.GetTypeFromProgID("CT.DispatcherViewer"));
+                        Viewer.ShowViewer(ref ret);
+                    }
+                    catch (System.Exception)
+                    {
+                        MessageBox.Show("Открыто несколько окон E3.Series\n" +
+                            "Закройте 'лишние' окна или установите E3.Dispatcher" +
+                            "\n\nВыход из программы");
+                    }
+
+                    if (ret == null)
+                    {
+                        MessageBox.Show("Не выбрано окно E3.Series" +
+                            "\n\nВыход из программы");
+                        System.Environment.Exit(0);
+                    }
+                    else return (e3Application)ret;
+                    break;
+            }
+
+            return null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.IndexOf(listBox1.SelectedItem);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AddToListBox();
+        }
+
+        private void Ok_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        
+    }
+
+}
